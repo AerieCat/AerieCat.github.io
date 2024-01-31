aa1e70dc0a18c7a08016ddbc9cfd67e2488f5fd1
BuildTools <unconfigured@null.spigotmc.org>
2024-01-31 15:19:20 -0500

    e

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/lgbt-rights/:state', async (req, res) => {
-  const state = req.params.state;
+  const state = req.params.state.replace(/ /g, '_');
   const url = `https://en.wikipedia.org/wiki/LGBT_rights_in_${state}#Summary_table`;
   try {
     const response = await nodeFetch(url);
