4c5b6fae1fe8a1bfa557d231118096fe3cc642fa
BuildTools <unconfigured@null.spigotmc.org>
2024-01-31 14:33:48 -0500

    e

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const fetch = require('node-fetch');
+import express from 'express';
+import fetch from 'node-fetch';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
