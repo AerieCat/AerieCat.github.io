53531c420b4b76faecdf18b99969a90f1a2732fe
BuildTools <unconfigured@null.spigotmc.org>
2024-01-31 14:42:39 -0500

    e

--- server.mjs
+++ server.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 import fetch from 'node-fetch';
+const { default: nodeFetch } = fetch;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,7 +9,7 @@ app.get('/lgbt-rights/:state', async (req, res) => {
   const state = req.params.state;
   const url = `https://en.wikipedia.org/wiki/LGBT_rights_in_${state}#Summary_table`;
   try {
-    const response = await fetch(url);
+    const response = await nodeFetch(url);
     const html = await response.text();
     res.send(html);
   } catch (error) {
